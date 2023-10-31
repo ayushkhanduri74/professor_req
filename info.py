0dc5a72d5608799b73a45e9228c87a5f4f0487b4
sahil sharma <113684157+ayushkhanduri78@users.noreply.github.com>
2023-10-31 15:15:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,12 @@
 import re, time
 from os import environ
 from Script import script 
+import asyncio
+import json
+from collections import defaultdict
+from typing import Dict, List, Union
+from pyrogram import Client
+from time import time
 
 id_pattern = re.compile(r'^.\d+$')
 
@@ -30,6 +36,9 @@ auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
+reqst_channel = environ.get('REQST_CHANNEL_ID')
+REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
+REQST_CHANNEL_ID = -1002048270121
 
 # MongoDB information
 DATABASE_URL = environ.get('DATABASE_URL', "")
